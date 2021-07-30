c3abd7e7a333dfd4314e566cbf9aaab8437f2667
Jan Barášek <janbarasek@gmail.com>
2021-07-30 21:27:13 +0200

    Add DIC extension.

---
+++ src/ShopDeliveryExtension.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Baraja\Shop\Delivery;
+
+
+use Baraja\Doctrine\ORM\DI\OrmAnnotationsExtension;
+use Nette\DI\CompilerExtension;
+
+final class ShopDeliveryExtension extends CompilerExtension
+{
+	public function beforeCompile(): void
+	{
+		$builder = $this->getContainerBuilder();
+		OrmAnnotationsExtension::addAnnotationPathToManager($builder, 'Baraja\Shop\Delivery\Entity', __DIR__ . '/Entity');
+	}
+}
